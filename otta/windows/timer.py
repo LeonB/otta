390ab748c2ad0b5c60bfee192790ee80aa093e4b
Leon Bogaert <leon@tim-online.nl>
2012-10-09 23:32:58 +0200

    Custom cellrenderer geprobeerd

--- otta/windows/timer.py
+++ otta/windows/timer.py
@@ -1,17 +1,21 @@
 from gi.repository import Gtk
+from gi.repository import GObject
 import otta
+from otta.models import Project, Task
 
 class Timer(otta.Window):
 	app = None
+	projects = []
 
 	def __init__(self, app):
 		otta.Window.__init__(self, 'timer.glade', 'windowTimer')
+
+		for project in self.get_projects():
+			self.projects.append(project)
+
 		self.app = app
 		self.setup_completions()
-
-		project_widget = self.get_widget('comboboxProject')
-		for project in self.app.backend.get_projects().keys():
-			project_widget.append_text(project)
+		self.setup_project_widget()
 
 	def setup_completions(self):
 		self.setup_projects_completion()
@@ -19,12 +23,8 @@ class Timer(otta.Window):
 
 	# fill a liststore with project names
 	def setup_projects_completion(self):
-		liststore = Gtk.ListStore(str)
-		for project in self.app.backend.get_projects():
-			liststore.append([project])
-
 		widget = self.get_widget('entrycompletionProjects')
-		widget.set_model(liststore)
+		widget.set_model(self.project_store())
 		widget.set_match_func(self.full_text_search, None)
 
 	# fill a liststore with task names
@@ -37,6 +37,28 @@ class Timer(otta.Window):
 		widget.set_model(liststore)
 		widget.set_match_func(self.full_text_search, None)
 
+	def setup_project_widget(self):
+		project_widget = self.get_widget('comboboxProject')
+		project_widget.set_model(self.project_store())
+
+		renderer = project_widget.get_cells()[0]
+		project_widget.set_cell_data_func(renderer, self.project_cell_data, None)
+
+	def project_cell_data(self, widget, cell, liststore, iter, user_data):
+		project = liststore.get_value(iter, 0)
+		cell.set_property('text', project.title)
+
+	def project_store(self):
+		store = Gtk.ListStore(GObject.TYPE_PYOBJECT)
+		for project in self.projects:
+			store.append([project])
+
+		return store
+
+	def get_projects(self):
+		for project in Project.select():
+			yield project
+
 	# Callback method for GtkEntryCompletion
 	def full_text_search(self, completion, entrystr, iter, data):
 		model = completion.get_model()
@@ -46,8 +68,8 @@ class Timer(otta.Window):
 	def on_comboboxProject_changed(self, widget):
 		print 'on_comboboxProject_changed'
 		text = widget.get_active_text()
-		projects = self.app.backend.get_projects()
-		if not projects.has_key(text):
+		projects = self.projects
+		if text not in projects:
 			return
 
 		project = projects[text]
